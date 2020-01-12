5bbc38ba6697ad932c91a945fa0f08802d3ae269
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-12 20:53:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  Bundler.require(:default, ENV['SINATRA_ENV'])).
+  Bundler.require(:default, ENV['SINATRA_ENV']))
   configure :development do
   set :database, 'sqlite3:db/database.db'
 end
